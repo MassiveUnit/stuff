76f7d2b6f1467f0f4d0194a947cc9068a977f677
MassiveUnit <96709394+MassiveUnit@users.noreply.github.com>
2022-06-23 08:58:48 +0200

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -6,11 +6,13 @@ local points
 local Version = "1.3.1"	
 points = 350
 local ChatColor = BrickColor.new("Bright red")
-makefolder("MUHUB")
+
+	
 	
 	
 local FunkyFriday = 6447798030
 local RoBowling = 340227283
+local PLSDONATE = 
 	
 function SendNotify(msg)
     game.StarterGui:SetCore("SendNotification", {
@@ -44,6 +46,8 @@ local FFSection = FFSettings:NewSection("Funky Friday")
 local FFKeyBindSection = FFSettings:NewSection("Keybinds")
 local RBSettings = Window:NewTab("RoBowling")
 local RBSection = RBSettings:NewSection("RoBowling")
+local PLSDSettings = Window:NewTab("PLS DONATE")
+local PLSDSection = PLSDSettings:NewSection("PLS DONATE")
 
 if game.PlaceId == FunkyFriday then
 RBSection:NewLabel("Join RoBowling to use this Section")
@@ -201,6 +205,72 @@ RBSection:NewKeybind(
     workspace[plr.Name].BowlingBall.HitBox.Size = Vector3.new(6, 6, 6)
     end
 )
+--PLS DONATE--
+--------------
+elseif game.PlaceId == PLSDONATE then
+PLSDSection:NewButton(
+    "Anti-AFK Toggle"
+    "Makes you walk around the map in a loop"
+    function()
+    if toggle == false then
+	SendNotify("Anti-AFK is ON")
+       toggle = true
+local player = game.Players.LocalPlayer
+local character = player.Character
+local hum = character.Humanoid
+
+local one = Vector3.new(42,3,43)
+local two = Vector3.new(2,3,56)
+local three = Vector3.new(-17,3,90)
+local four = Vector3.new(-9,3,129)
+local five = Vector3.new(25,3,158)
+local six = Vector3.new(78,3,146)
+local seven = Vector3.new(96,3,102)
+local eight = Vector3.new(81,3,61)
+local part = Instance.new("Part")
+
+part.Parent = workspace
+part.Anchored = true
+part.CanCollide = false
+part.BrickColor = BrickColor.new("Bright red")
+part.Name = "ANTIAFKPART"
+part.Size = Vector3.new(1,10,1)
+
+while toggle == true do
+SendNotify("Looping Around the map")
+hum:MoveTo(one)
+part.Position = one
+wait(5)
+hum:MoveTo(two)
+part.Position = two
+wait(5)
+hum:MoveTo(three)
+part.Position = three
+wait(5)
+hum:MoveTo(four)
+part.Position = four
+wait(5)
+hum:MoveTo(five)
+part.Position = five
+wait(5)
+hum:MoveTo(six)
+part.Position = six
+wait(5)
+hum:MoveTo(seven)
+part.Position = seven
+wait(5)
+hum:MoveTo(eight)
+part.Position = eight
+wait(5)
+part:Destroy()
+end		
+    elseif toggle == true then
+       toggle = false
+    SendNotify("Anti-AFK is OFF")
+    workspace.ANTIAFKPART:Destroy()
+    end	
+    end
+)		
 
 local TabCredit = Window:NewTab("Credits")
 local CreditSection = TabCredit:NewSection("Credits")
